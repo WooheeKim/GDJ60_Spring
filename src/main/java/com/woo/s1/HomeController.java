cb46263b96f87ded39378575c6385f6874ef2c2a
KimWoohee <kimdngml12@naver.com>
2023-02-02 14:48:15 +0900

    1

--- src/main/java/com/woo/s1/HomeController.java
+++ src/main/java/com/woo/s1/HomeController.java
@@ -1,20 +1,16 @@
 package com.woo.s1;
 
-import java.nio.Buffer;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.woo.s1.army.Bullet;
-import com.woo.s1.army.Soldier;
 
 /**
  * Handles requests for the application home page.
