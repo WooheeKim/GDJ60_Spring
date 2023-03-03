89fdb43f686d6c746a1a0ff68b5b46a4d1e1b444
KimWoohee <kimdngml12@naver.com>
2023-03-03 11:20:11 +0900

    1

--- src/main/webapp/htmlJquery/jsStudy1.js
+++ src/main/webapp/htmlJquery/jsStudy1.js
@@ -7,6 +7,25 @@ const buttons = document.querySelectorAll(".buttons"); //document.getElementsByC
 
 const d1 = document.querySelector("#d1 > .buttons li");
 
+console.log('Old');
+for(let i=0;i<Array.length;i++) {
+    console.log(ar[i]);
+}
+
+console.log('For of');
+for(let a of ar) {
+    console.log(a);
+}
+
+console.log('For in');
+for(let j in ar);
+
+console.log('For each');
+ar.forEach(function(v, i, list){
+    console.log();
+});
+
+
 btn1.addEventListener("click", function(){
     console.log("btn1");
 });
@@ -16,4 +35,11 @@ for(let b of buttons) {
         console.log("buttons")
     });
 }
+
+alert(buttons);
+alert(ar);
+console.log(buttons);
+console.log(ar);
+
+buttons.forEach(function)
 
